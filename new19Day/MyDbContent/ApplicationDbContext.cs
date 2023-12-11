68d900804f3c6fbbd2e6cdde81374693e740599b
Fatmazadjali <maathermohammed77@gmail.com>
2023-12-11 15:35:10 +0400

    connect with database

---
+++ new19Day/MyDbContent/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Text;
+using System.Threading.Tasks;
+using new19Day.Models;
+
+namespace new19Day.MyDbContent
+{
+	internal class ApplicationDbContext : DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			base.OnConfiguring(optionsBuilder);
+			optionsBuilder.UseSqlServer("Data Source=Data Source=DESKTOP-RETJJ6I\\MSSQLS;Initial Catalog=MyDatabase;Integrated Security=True;Connect");
+		}
+
+
+		ApplicationDbContext db = new ApplicationDbContext();
+		public DbSet<Employee> Employees { get; set; }
+	}
+}
---
+++ new19Day/Program.cs
@@ -0,0 +1,13 @@
+﻿using new19Day.MyDbContent;
+
+namespace new19Day
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+			ApplicationDbContext context = new ApplicationDbContext();
+		}
+	}
+}
